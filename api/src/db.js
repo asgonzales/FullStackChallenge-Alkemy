de930f5aa91ba7b5f188614ff98fea025273ae6e
asgonzales <asgonzalesromani@gmail.com>
2022-09-20 22:45:07 -0300

    Add relations between models

--- api/src/db.js
+++ api/src/db.js
@@ -1,22 +1,32 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 const { DB_USER, DB_PASS, DB_HOST } = process.env;
-const User = require('./models/User.js');
-const Operation = require('./models/Operation.js');
-const Category = require('./models/Category.js');
+const user = require('./models/User.js');
+const operation = require('./models/Operation.js');
+const category = require('./models/Category.js');
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/budget`, {
     logging: false,
     native: false
 })
 
-User(sequelize);
-Operation(sequelize);
-Category(sequelize);
+//Conecto los modelos con la bd
+user(sequelize);
+operation(sequelize);
+category(sequelize);
 
+//Defino las relaciones entre los modelos
+const { User, Operation, Category } = sequelize.models
+
+Operation.hasOne(User, {foreignKey: 'id'})
+User.belongsTo(Operation, {foreignKey: 'id'})
+
+Operation.hasOne(Category, {foreignKey: 'id'})
+Category.belongsTo(Operation, {foreignKey: 'id'})
 
 
 
 module.exports = {
+    ...sequelize.models,
     conn: sequelize
 }
