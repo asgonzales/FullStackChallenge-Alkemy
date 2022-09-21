338b9b0fdcbf6d0e9f0793814541254572527f59
asgonzales <asgonzalesromani@gmail.com>
2022-09-21 14:39:44 -0300

    Configure redux

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,19 @@
+
+
+
+
+const initialState = {
+}
+
+
+export const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case '': return {
+            
+        }
+            
+    
+        default: return state
+    }
+}
