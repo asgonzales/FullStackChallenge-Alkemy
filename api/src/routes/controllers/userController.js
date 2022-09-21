3d093f2154c772073d8f0e0bb434bd5374e15938
asgonzales <asgonzalesromani@gmail.com>
2022-09-21 00:03:30 -0300

    Fix registerUser controller

--- api/src/routes/controllers/userController.js
+++ api/src/routes/controllers/userController.js
@@ -11,14 +11,14 @@ const registerUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const exist = await User.findOne({where: {email: email}})
-        if(exist) res.status(400).json({error: 'User already exist'})
+        if(exist) return res.status(400).json({error: 'User already exist'})
     
         const userCreated = await User.create({email, password})
     
-        res.status(200).json(userCreated)
+        return res.status(200).json(userCreated)
 
     } catch(err) {
-        res.status(404).json({error: err.message})
+        return res.status(404).json({error: err.message})
     }
 
 }
