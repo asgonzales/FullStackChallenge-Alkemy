ec768968b1a4c03ff170e712832bd7c0d41267cd
asgonzales <asgonzalesromani@gmail.com>
2022-09-25 00:01:53 -0300

    Add redirect in Landing

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import style from './Landing.module.css';
 
 
@@ -16,13 +16,14 @@ export default function Landing () {
     return (
         <div className={style.contLanding}>
             <div>
-                <h1>LALALA</h1>
+                <h1>Welcome</h1>
             </div>
             <div>
                 <button>
                     <Link to='/signin'>Ingresar</Link>
                 </button>
             </div>
+            <Navigate to='/signin' />
         </div>
     )
 }
