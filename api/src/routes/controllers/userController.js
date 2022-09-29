f2b12288105c9de73b1ddae3763d5fbb15bd348e
asgonzales <asgonzalesromani@gmail.com>
2022-09-29 13:18:56 -0300

    Add Google Sign In in back

--- api/src/routes/controllers/userController.js
+++ api/src/routes/controllers/userController.js
@@ -2,6 +2,7 @@ const { User } = require('../../db.js');
 const jwt = require('jsonwebtoken');
 const { KEY_JWT } = process.env;
 const bcrypt = require('bcrypt');
+const jwtscnd = require('jwt-decode');
 
 
 
@@ -43,6 +44,24 @@ const loginUser = async (req, res) => {
         return res.status(400).json({error: err.message})
     }
 }
+const signGoogle = async (req, res) => {
+    const { credential } = req.body
+    try {
+        // console.log(credential)
+        const { email, given_name } = jwtscnd(credential)
+        const newUser = {email, name: given_name}
+        // console.log(email)
+        const user = await User.findOrCreate({ where: { email: email }, defaults: newUser })
+        // console.log('NUEVO USUARIO', user)
+        const token = jwt.sign({ id: user[0].dataValues.id }, KEY_JWT, {expiresIn: '3h'})
+        res.cookie('token', token, {sameSite: 'none', secure: true })
+        return res.status(200).json({ token: token })
+    } catch (err) {
+        // console.log(err)
+        return res.status(400).json({ error: err.message })
+    }
+}
+
 const signOutUser = async (req, res) => {
     try {
         res.cookie('token', 0, { expires: new Date(0)})
@@ -57,5 +76,6 @@ const signOutUser = async (req, res) => {
 module.exports = {
     registerUser,
     loginUser,
+    signGoogle,
     signOutUser
 }
