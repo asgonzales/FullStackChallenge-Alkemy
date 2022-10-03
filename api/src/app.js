51a10d74bf2039217f069ec1e11add876228e4d7
asgonzales <asgonzalesromani@gmail.com>
2022-10-03 15:29:00 -0300

    Update CORS config

--- api/src/app.js
+++ api/src/app.js
@@ -13,17 +13,20 @@ const server = express();
 
 server.use(morgan('dev'));
 server.use(express.json());
+console.log(CORS_ORIGIN)
 server.use(cors({
     origin: `${CORS_ORIGIN}`,
+    methods: 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
+    allowedHeaders: 'X-Requested-With, content-type',
     credentials: true
 }));
-server.use( (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    next()
-})
+// server.use( (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', CORS_ORIGIN)
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     next()
+// })
 server.use(cookieParser());
 server.use('/', routes);
 
