72af37d288f7bb504a619f5594c77adcc5524282
asgonzales <asgonzalesromani@gmail.com>
2022-10-03 16:41:46 -0300

    Add options response

--- api/src/app.js
+++ api/src/app.js
@@ -1,35 +1,36 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan')
 const { routes } = require('./routes/index.js');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const { CORS_ORIGIN } = process.env
+const { CORS_ORIGIN } = process.env;
 
 
 
 
 const server = express();
 
-server.use(cors())
-// server.use(cors({
-//     origin: 'https://finnapp.vercel.app',
-//     methods: 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
-//     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token',
-//     credentials: true
-// }));
+// server.use(cors())
+server.use(cors({
+    origin: `${CORS_ORIGIN}`,
+    methods: 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token',
+    credentials: true
+}));
 
 server.use(morgan('dev'));
 server.use(express.json());
 server.use(cookieParser());
 
-server.use( (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://finnapp.vercel.app')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    next()
-})
+// server.use( (req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token')
+//     res.header('Access-Control-Allow-Credentials', 'true')
+//     next()
+// })
+
 server.use('/', routes);
 
 
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const { Router } = require('express');
 const { operationRoutes } = require('./operationsRoutes.js');
 const { userRoutes } = require('./userRoutes.js');
 const { categoryRoutes } = require('./categoryRoutes.js');
+const cors = require('cors');
+const { CORS_ORIGIN } = process.env;
 
 
 
@@ -9,6 +12,12 @@ const { categoryRoutes } = require('./categoryRoutes.js');
 
 
 const routes = Router();
+routes.options('*', cors({
+    origin: `${CORS_ORIGIN}`,
+    methods: 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token',
+    credentials: true
+}))
 
 routes.use('/operation', operationRoutes)
 routes.use('/user', userRoutes)
