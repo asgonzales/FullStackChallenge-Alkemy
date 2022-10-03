db075bd39b8d3135b6b6d7e8f9a9468f3fff2000
asgonzales <asgonzalesromani@gmail.com>
2022-10-03 17:40:54 -0300

    Restore cors config

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,7 @@
-require('dotenv').config();
 const { Router } = require('express');
 const { operationRoutes } = require('./operationsRoutes.js');
 const { userRoutes } = require('./userRoutes.js');
 const { categoryRoutes } = require('./categoryRoutes.js');
-const cors = require('cors');
-const { CORS_ORIGIN } = process.env;
 
 
 
@@ -12,15 +9,6 @@ const { CORS_ORIGIN } = process.env;
 
 
 const routes = Router();
-routes.options('*', cors({
-    origin: `${CORS_ORIGIN}`,
-    methods: 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token',
-    credentials: true
-}), (req, res) => {
-    console.log('OPTIONSSSS')
-    return res.status(200).json({ success: 'success'})
-})
 
 routes.use('/operation', operationRoutes)
 routes.use('/user', userRoutes)
